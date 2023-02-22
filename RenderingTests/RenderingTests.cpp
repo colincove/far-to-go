38d4b4e808b27e77d8763d15cf6fee25ad7da5be
COLINSP8\colin <cove.colin@gmail.com>
2023-02-21 20:42:23 -0800

    Adding Projects

---
+++ RenderingTests/RenderingTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace RenderingTests
+{
+	TEST_CLASS(RenderingTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
